36aaff45528b46fc93d450b5f4df0a88dc4e9f28
Diogo Martins de Assis <diogomrtsa@gmail.com>
2024-07-18 19:56:17 -0300

    feat: create server websocket module

---
+++ socket-server-go/server/server.go
@@ -0,0 +1,55 @@
+package server
+
+import (
+	"errors"
+	"flag"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var (
+	// Message about Error generated when the server try to read messages.
+	ErrReadMessageWebSocket = "error on reading messages"
+	// Message about Error generated when the client try to connect.
+	ErrUpgradeWebSocketProtocol = "error on upgrade http connection"
+	// Error generated when the application try to start the server.
+	ErrStartWebSocketServer = errors.New("error on starting websocket server")
+)
+var upgrader = websocket.Upgrader{}
+var addr = flag.String("addr", "localhost:8080", "http service address")
+
+// Start WebSocket Server or return an error.
+func Start() error {
+	flag.Parse()
+	log.SetFlags(0)
+	http.HandleFunc("/echo", echo)
+	err := http.ListenAndServe(*addr, nil)
+	if err != nil {
+		return ErrStartWebSocketServer
+	}
+	return nil
+}
+
+// Responsible to upgrade Http connections and read messages.
+func echo(w http.ResponseWriter, r *http.Request) {
+	c, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Fatalln(ErrUpgradeWebSocketProtocol)
+	}
+	defer c.Close()
+	for {
+		mt, message, err := c.ReadMessage()
+		if err != nil {
+			log.Fatalln(ErrReadMessageWebSocket)
+			break
+		}
+		log.Printf("recv: %s", message)
+		err = c.WriteMessage(mt, message)
+		if err != nil {
+			log.Println("write:", err)
+			break
+		}
+	}
+}
