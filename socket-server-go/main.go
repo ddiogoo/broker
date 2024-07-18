0df8935949313ba4cc09ecbed528489226208f2d
Diogo Martins de Assis <diogomrtsa@gmail.com>
2024-07-18 19:56:25 -0300

    feat: call websocket server

--- socket-server-go/main.go
+++ socket-server-go/main.go
@@ -1,8 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"github.com/ddiogoo/broker/tree/master/socket-server-go/env"
+	"github.com/ddiogoo/broker/tree/master/socket-server-go/server"
+)
 
 // Method responsible for initializing our application.
 func main() {
-	fmt.Println("hello, world!")
+	env.Config()
+	err := server.Start()
+	if err != nil {
+		panic(err.Error())
+	}
 }
