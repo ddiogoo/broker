657591a56685a2c6069a148c7a26290178f12bb5
Diogo Martins de Assis <diogomrtsa@gmail.com>
2024-07-25 21:21:20 -0300

    refactor: changing method invoke

--- socket-server-go/main.go
+++ socket-server-go/main.go
@@ -2,13 +2,19 @@ package main
 
 import (
 	"github.com/ddiogoo/broker/tree/master/socket-server-go/env"
+	"github.com/ddiogoo/broker/tree/master/socket-server-go/mq"
 	"github.com/ddiogoo/broker/tree/master/socket-server-go/server"
 )
 
 // main is the entry point of the application.
 func main() {
 	env.Config()
-	err := server.Start()
+	_, err := mq.ConnectToNats()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	err = server.Start()
 	if err != nil {
 		panic(err.Error())
 	}
