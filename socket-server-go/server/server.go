1b8aeb3d7a91e107bb328a03a84be4a05b5067bc
Diogo Martins de Assis <diogomrtsa@gmail.com>
2024-07-30 14:23:19 -0300

    feat: create a parameter that receive a nats conn

--- socket-server-go/server/server.go
+++ socket-server-go/server/server.go
@@ -5,19 +5,20 @@ import (
 	"net/http"
 
 	"github.com/ddiogoo/broker/tree/master/socket-server-go/client"
+	"github.com/ddiogoo/broker/tree/master/socket-server-go/mq"
 )
 
 // addr is a pointer to a string that represents the address of the server.
 var addr = flag.String("addr", ":8080", "http service address")
 
 // Start is responsible for starting the server.
-func Start() error {
+func Start(n *mq.Nats) error {
 	flag.Parse()
 	hub := client.NewHub()
 	go hub.Run()
 
 	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
-		client.ServeWs(hub, w, r)
+		client.ServeWs(n, hub, w, r)
 	})
 	err := http.ListenAndServe(*addr, nil)
 
