ccf3ea69bd82e76d7bb2e5db16bcb0f8ebb602af
Diogo Martins de Assis <diogomrtsa@gmail.com>
2024-08-01 21:48:51 -0300

    refactor: refact routes

--- key-manager-go/routes/routes.go
+++ key-manager-go/routes/routes.go
@@ -1,9 +1,13 @@
 package routes
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/ddiogoo/broker/tree/master/key-manager-go/ctx"
+	"github.com/ddiogoo/broker/tree/master/key-manager-go/dto"
+	"github.com/ddiogoo/broker/tree/master/key-manager-go/generator"
+	"github.com/ddiogoo/broker/tree/master/key-manager-go/model"
 	"github.com/gin-gonic/gin"
 )
 
@@ -13,20 +17,17 @@ func ConfigureRoutes(r *gin.Engine, db *ctx.KeyManagerDatabase) {
 			"message": "pong",
 		})
 	})
-}
-
-/*
-	r.GET("/ping", func(c *gin.Context) {
-		var test test.TestDto
-		if err := c.BindJSON(&test); err != nil {
+	r.POST("", func(c *gin.Context) {
+		var keyDto dto.KeyDto
+		if err := c.BindJSON(&keyDto); err != nil {
 			c.JSON(http.StatusBadRequest, gin.H{
-				"message": "invalid body",
+				"message": "invalid body to KeyDto",
 			})
 			return
 		}
-		c.JSON(http.StatusOK, gin.H{
-			"message": "body is ok",
-		})
-		fmt.Println(test.Email)
+		key := model.NewKey(keyDto.Email, generator.GenerateApiKey(), "/ping")
+		fmt.Println("Email: " + key.Email)
+		fmt.Println("ApiKey: " + key.ApiKey)
+		fmt.Println("Routes: " + key.Routes)
 	})
-*/
+}
