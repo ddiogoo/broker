55d50f68ffed2911fe463c57d85b26d498b98df3
Diogo Martins de Assis <diogomrtsa@gmail.com>
2024-08-03 15:53:14 -0300

    feat: add database builder on databasemanager

--- key-manager-go/database/database.go
+++ key-manager-go/database/database.go
@@ -3,16 +3,19 @@ package database
 import (
 	"database/sql"
 	"os"
+	"reflect"
 
+	"github.com/ddiogoo/broker/tree/master/key-manager-go/builder"
 	_ "github.com/lib/pq"
 )
 
-type KeyManagerDatabase struct {
-	db *sql.DB
+type DatabaseManager struct {
+	db        *sql.DB
+	dbBuilder *builder.DatabaseQueryBuilder
 }
 
 // Close closes the connection with database.
-func (k *KeyManagerDatabase) Close() {
+func (k *DatabaseManager) Close() {
 	err := k.db.Close()
 	if err != nil {
 		panic(err.Error())
@@ -20,7 +23,7 @@ func (k *KeyManagerDatabase) Close() {
 }
 
 // Close closes the connection with database.
-func (k *KeyManagerDatabase) Ping() error {
+func (k *DatabaseManager) Ping() error {
 	err := k.db.Ping()
 	if err != nil {
 		return err
@@ -28,17 +31,34 @@ func (k *KeyManagerDatabase) Ping() error {
 	return nil
 }
 
-// NewKeyManagerDatabase creates a new instance of KeyManagerDatabase.
-func NewKeyManagerDatabase() (*KeyManagerDatabase, error) {
+// Table create all tables.
+func (k *DatabaseManager) Table() error {
+	k.dbBuilder.BuildCreateTable()
+	for _, v := range k.dbBuilder.Tables {
+		_, err := k.db.Exec(v)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// NewDatabaseManager creates a new instance of DatabaseManager.
+func NewDatabaseManager(types []reflect.Type) (*DatabaseManager, error) {
 	connStr := func() string {
 		if os.Getenv("GIN_RUN_MODE") == "debug" {
 			return os.Getenv("CONN_PGDB_DEBUG_DB")
 		}
 		return os.Getenv("CONN_PGDB_RELEASE_DB")
 	}()
+
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
 		return nil, err
 	}
-	return &KeyManagerDatabase{db: db}, nil
+
+	return &DatabaseManager{
+		db:        db,
+		dbBuilder: builder.NewDatabaseQueryBuilder(types),
+	}, nil
 }
