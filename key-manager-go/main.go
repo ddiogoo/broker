3a68b40d818966368bc989c58c64c04a0aa9acf8
Diogo Martins de Assis <diogomrtsa@gmail.com>
2024-08-05 19:04:12 -0300

    feat: add anonymous function to get the correct conn string

--- key-manager-go/main.go
+++ key-manager-go/main.go
@@ -22,7 +22,12 @@ func main() {
 	// MongoDB
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(func() string {
+		if os.Getenv("GIN_RUN_MODE") == "debug" {
+			return os.Getenv("CONN_STRING_MONGODB_DEBUG")
+		}
+		return os.Getenv("CONN_STRING_MONGODB_RELEASE")
+	}()))
 	if err != nil {
 		panic(err)
 	}
