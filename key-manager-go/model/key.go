a639a7419ce0d077d26bb64e46d03bcea96c50cc
Diogo Martins de Assis <diogomrtsa@gmail.com>
2024-08-08 20:15:45 -0300

    refactor: change key model

--- key-manager-go/model/key.go
+++ key-manager-go/model/key.go
@@ -1,16 +1,16 @@
 package model
 
 type Key struct {
-	Email  string `json:"email"`
-	ApiKey string `json:"apiKey"`
-	Route  string `json:"route"`
+	Email  string            `json:"email"`
+	ApiKey string            `json:"apiKey"`
+	Routes []RoutePermission `json:"routes"`
 }
 
 // NewKey create an instance of Key struct.
-func NewKey(email, apiKey, route string) *Key {
+func NewKey(email, apiKey string, routes []RoutePermission) *Key {
 	return &Key{
 		Email:  email,
 		ApiKey: apiKey,
-		Route:  route,
+		Routes: routes,
 	}
 }
